ccfad9ac68f7e288f2a14b49301df4d1b5f05c97
Jumatemory <*Case#255>
2020-03-12 00:44:42 -0400

    My name is Juma:

--- src/test/java/day7/MyClass.java
+++ src/test/java/day7/MyClass.java
@@ -1,4 +1,8 @@
 package day7;
 
 public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("hello word ");
+        System.out.println("my name is kHan ");
+    }
 }
